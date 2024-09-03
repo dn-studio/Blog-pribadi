9016ff884aaaecafd7de79eb012166881408cdf0
dn-studio <kadekdion18@gmail.com>
2024-09-03 14:29:42 +0800

    first commit

---
+++ index-dark.php
@@ -0,0 +1,425 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from wpkixx.com/html/Aris/index-dark.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 27 Aug 2024 11:41:33 GMT -->
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=utf8mb4" /><!-- /Added by HTTrack -->
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<title>Dion | Personal Blog</title>
+	<link rel="icon" href="img/fav 1.png" type="image/png" sizes="16x16">
+	<link rel="stylesheet" href="css/icofont.min.css">
+	<link rel="stylesheet" href="css/foundation.min.css">
+	<link rel="stylesheet" href="css/fullpage.css">
+	<link rel="stylesheet" href="css/perfect-scrollbar.min.css">
+	<link rel="stylesheet" href="css/modal-animated.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/responsive.css">
+</head>
+
+<body class="dark-bg">
+
+	<div class="page-loader" id="page-loader">
+		<div class="lds-ellipsis">
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+		<span>Loading...</span>
+	</div><!-- page loader -->
+
+	<div class="page-cover" id="s-cover">
+		<!--<div class="cover-bg">
+		<img src="img/bg-default.jpg" alt="">
+	 </div>-->
+		<!--<div id="" class="blackish">
+		<div id="home" class="home-bg" data-stellar-background-ratio="0.6">
+			<div id="bg-youtube" class="player" data-property="{videoURL:'https://www.youtube.com/watch?v=2uQ58Xwx1V4', 
+				containment:'#home', 
+				showControls:false, 
+				autoPlay:true, 
+				loop:true, 
+				mute:true, 
+				startAt:0, 
+				opacity:1, 
+				addRaster:true,
+				quality:'default'}">
+			</div>
+		</div>
+	  </div>--><!-- background youtube video -->
+	</div>
+
+	<nav class="quick-link count-4 nav-left">
+		<div class="logo">
+			<a href="#home">
+				<img src="img/logo 1.png" alt="Logo">
+			</a>
+		</div>
+		<ul id="qmenu" class="qmenu">
+			<li data-menuanchor="home">
+				<a href="#home" class=""><i class="icofont-bow"></i>
+				</a>
+				<span class="title">Home</span>
+			</li>
+			<li data-menuanchor="about-us">
+				<a href="#about-us"><i class="icofont-info-circle"></i>
+				</a>
+				<span class="title">About Us</span>
+			</li>
+			<li data-menuanchor="services">
+				<a href="#services"><i class="icofont-architecture-alt"></i>
+				</a>
+				<span class="title">Layanan</span>
+			</li>
+			<li data-menuanchor="portfolio">
+				<a href="#portfolio"><i class="icofont-slidshare"></i>
+				</a>
+				<span class="title">portfolio</span>
+			</li>
+			<li data-menuanchor="blog">
+				<a href="#blog"><i class="icofont-brightkite"></i>
+				</a>
+				<span class="title">blog</span>
+			</li>
+			<li data-menuanchor="contact">
+				<a href="#contact"><i class="icofont-map-pins"></i>
+				</a>
+				<span class="title">Contact</span>
+			</li>
+		</ul>
+	</nav><!-- side nav -->
+
+	<div class="pane-when " id="s-when">
+		<div class="content">
+			<div class="avatar">
+				<div class="elem-center">
+					<div class="">
+						<img src="img/student.png" alt="">
+					</div>
+				</div>
+			</div>
+			<div class="bottombar">
+				<span>&copy; Copyright 2022 By Ganadev Com_Galeri</span>
+			</div>
+		</div>
+	</div><!-- student image -->
+
+	<main class="page-main" id="mainpage">
+		<div class="section page-home page page-cent" id="s-home">
+			<section class="content">
+				<header class="header">
+					<div class="h-left">
+						<h2>I Kadek Dion A.w <h2>
+					</div>
+					<div class="aris-meta">
+						<h3>HI, I am a <span></span></h3>
+						<p>
+							Saya adalah I Kadek Dion Arya Widarma, Saya seorang fotografer, desainer, dan videografer dengan bakat
+							di bidang visual. Di blog ini, saya berbagi karya dan inspirasi dari perjalanan kreatif saya.
+							Selamat menikmati! </p>
+						<a href="#contact" title="" class="main-btn">Contact Now</a>
+						<a href="#" title="" class="main-btn">Download CV</a>
+					</div>
+				</header>
+			</section>
+			<!-- Scroll down button -->
+			<footer class="p-footer p-scrolldown">
+				<a href="#about-us">
+					<div class="arrow-d">
+						<div class="before"></div>
+						<div class="after"></div>
+						<div class="circle bounce"><i class="icofont-wireless-mouse"></i></div>
+					</div>
+				</a>
+			</footer>
+		</div>
+		<div class="section page-about page page-cent" id="s-about-us">
+			<section class="content">
+				<header class="p-title">
+					<h3>
+						<i class="icofont-info-circle"></i> About Us
+						<span class="animated-bar"></span>
+					</h3>
+					<h4 class="subhead">foto adalah suara hati yang dilihat oleh mata</h4>
+				</header>
+				<div class="about-meta">
+					<p>
+						Blog ini adalah tempat di mana saya berbagi perjalanan kreatif saya, proyek-proyek terbaru, dan tips serta trik yang saya pelajari di sepanjang jalan. Terima kasih telah mengunjungi, dan semoga Anda menemukan inspirasi dalam setiap karya yang saya bagikan </p>
+					<h5>Little About:</h5>
+					<ul class="about-info">
+						<li><span>Tanggal Lahir:</span> -</li>
+						<li><span>Email:</span> Kadekdion18@gmail.com</li>
+						<li><span>Umur:</span> -</li>
+						<li><span>Wa: </span> 081237178315</li>
+						<li><span>Tempat Tinggal:</span> Singaraja</li>
+						<li><span>Freelance</span> Tersedia</li>
+					</ul>
+					<h5>Kehalian:</h5>
+					<ul class="list-experties">
+						<li><i class="icofont-check-circled"></i> Adobe Ilustrator</li>
+						<li><i class="icofont-check-circled"></i> Adobe Photoshop</li>
+						<li><i class="icofont-check-circled"></i> adobe Premiere Pro</li>
+						<li><i class="icofont-check-circled"></i> Capcut</li>
+						<li><i class="icofont-check-circled"></i> HTML</li>
+						<li><i class="icofont-check-circled"></i> CSS</li>
+						<li><i class="icofont-check-circled"></i> Blender</li>
+					</ul>
+				</div>
+			</section>
+		</div>
+		<div class="section page-register page page-cent" id="s-services">
+			<section class="content">
+				<header class="p-title">
+					<h3>
+						<i class="icofont-architecture-alt"></i> Layanan Kami
+						<span class="animated-bar"></span>
+					</h3>
+				</header>
+				<div class="services">
+					<div class="service-box">
+						<i class="icofont-bebo"></i>
+						<h4>Desainner</h4>
+						<p>
+							Membuka Layanan Jasa Desain, Baju, Mug, Poster, DLL
+						</p>
+					</div>
+				</div>
+				<div class="services">
+					<div class="service-box">
+						<i class="icofont-heart-alt"></i>
+						<h4>PHOTOGRAFER</h4>
+						<p>
+							Membuka Layanan Jasa Photografer, Wedding, Wisuda, Event , DLL
+						</p>
+					</div>
+				</div>
+				<div class="services">
+					<div class="service-box">
+						<i class="icofont-ui-folder"></i>
+						<h4>VIDEOGRAFER</h4>
+						<p>
+							Membuka Layanan Jasa Videografer
+						</p>
+					</div>
+				</div>
+				<div class="services">
+					<div class="service-box">
+						<i class="icofont-edit"></i>
+						<h4>UI & UX</h4>
+						<p>
+						Membuka Layanan Jasa Desain UI & UX 
+						</p>
+					</div>
+				</div>
+			</section>
+		</div>
+		<div class="section page-register page page-cent" id="s-portfolio">
+			<section class="content">
+				<header class="p-title">
+					<h3>
+						<i class="icofont-slidshare"></i> PORTOFOLIO
+						<span class="animated-bar"></span>
+					</h3>
+					<h4 class="subhead">Lihat karya terbaru kami</h4>
+				</header>
+				<div class="our-work">
+					<div class="work-box">
+						<img src="img/work1.png" alt="">
+						<div class="work-text">
+							<span>Desain</span>
+							<h4><a class="popup six" href="#" title="">Baju Pelatih Futsal</a></h4>
+						</div>
+					</div>
+					<div class="work-box">
+						<img src="img/work2.png" alt="">
+						<div class="work-text">
+							<span>Desain</span>
+							<h4><a class="popup six" href="#" title="">Baju GK Futsal</a></h4>
+						</div>
+					</div>
+					<div class="work-box">
+						<img src="img/work3.png" alt="">
+						<div class="work-text">
+							<span>Desain</span>
+							<h4><a class="popup six" href="#" title="">Baju Pemain Futsal</a></h4>
+						</div>
+					</div>
+					<div class="work-box">
+						<img src="img/work4.png" alt="">
+						<div class="work-text">
+							<span>Desain</span>
+							<h4><a class="popup six" href="#" title="">Stiker Club</a></h4>
+						</div>
+					</div>
+					<div class="work-box">
+						<img src="img/work5.png" alt="">
+						<div class="work-text">
+							<span>Desain</span>
+							<h4><a class="popup six" href="#" title="">Baju Pembina Osis 1</a></h4>
+						</div>
+					</div>
+					<div class="work-box">
+						<img src="img/work6.png" alt="">
+						<div class="work-text">
+							<span>Desain</span>
+							<h4><a class="popup six" href="#" title="">Baju Pembina Osis 2</a></h4>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+		<div class="section page-register page page-cent" id="s-blog">
+			<section class="content">
+				<header class="p-title">
+					<h3>
+						<i class="icofont-brightkite"></i> Blog ( Coming soon )
+						<span class="animated-bar"></span>
+					</h3>
+				</header>
+				<div class="blog">
+					<div class="blog-box">
+						<figure><img src="img/blog1.jpg" alt=""><span>Coming soon</span></figure>
+						<h4><a class="popup six" href="#" title="">Coming soon</a></h4>
+					</div>
+					<div class="blog-box">
+						<figure><img src="img/blog2.jpg" alt=""><span>Coming soon</span></figure>
+						<h4><a class="popup six" href="#" title="">Coming soon</a></h4>
+					</div>
+					<div class="blog-box">
+						<figure><img src="img/blog3.jpg" alt=""><span>Coming soon</span></figure>
+						<h4><a class="popup six" href="#" title="">Coming soon</a></h4>
+					</div>
+					<div class="blog-box">
+						<figure><img src="img/blog4.jpg" alt=""><span>Coming soon</span></figure>
+						<h4><a class="popup six" href="#" title="">Coming soon</a></h4>
+					</div>
+				</div>
+			</section>
+		</div>
+		<div class="section page-contact page page-cent  bg-color" data-bgcolor="rgba(95, 25, 208, 0.88)s" id="s-contact">
+			<div class="slide" id="s-information" data-anchor="information">
+				<section class="content">
+					<header class="p-title">
+						<h3>
+							<i class="icofont-map-pins"></i> Kontak Kami
+							<span class="animated-bar"></span>
+						</h3>
+					</header>
+					<!-- Begin Of Page SubSction -->
+					<div class="contact">
+						<div class="row">
+							<div class="medium-6 columns">
+								<ul class="contact-detail">
+									<li>
+										<h4>Email</h4>
+										<p><a href="mailto://contact@mail.com">kadekdion18@gmail.com</a></p>
+									</li>
+									<li>
+										<h4>Alamat</h4>
+										<p>Jalan Raya Desa Kalibukbuk
+											<br>Singaraja
+										</p>
+									</li>
+									<li>
+										<h4>WA</h4>
+										<p>081237178315</p>
+									</li>
+									<li class="show-for-medium-up">
+										<h4>Follow Me</h4>
+										<!-- Begin of Social links -->
+										<div class="socialnet">
+											<a href="#"><i class="icofont-facebook"></i></a>
+											<a href="#"><i class="icofont-instagram"></i></a>
+											<a href="#"><i class="icofont-twitter"></i></a>
+											<a href="#"><i class="icofont-pinterest"></i></a>
+											<a href="#"><i class="icofont-tumblr"></i></a>
+										</div>
+										<!-- End of Social links -->
+									</li>
+								</ul>
+							</div>
+							<div class="medium-6 columns contact-area">
+								<h4>Write A Message</h4>
+								<div>
+									<div id="message"></div>
+									<form class="message" method="post" action="https://wpkixx.com/html/Aris/contact.php" id="contactform">
+										<div class="fields">
+											<div class="input">
+												<input id="name" type="text" placeholder="Your Name" required>
+											</div>
+										</div>
+										<div class="fields">
+											<div class="input">
+												<input id="email" type="email" placeholder="Email Address" required>
+											</div>
+										</div>
+										<div class="fields">
+											<textarea id="comments" placeholder="Message ..." rows="3" required></textarea>
+											<div class="buttons">
+												<button id="submit" type="submit" class="main-btn">Send Now</button>
+												<img src="img/ajax-loader.gif" class="loader" alt="ajax-loader.gif">
+											</div>
+											<!--<div class="g-recaptcha" data-sitekey="6LeBpOgUAAAAAJwweRt7FijmJ7nb1Wm5VTyn9MoR"></div>-->
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- End of page SubSection -->
+				</section>
+			</div>
+		</div>
+	</main><!-- main content area -->
+
+	<div id="modal-container">
+		<div class="modal-background">
+			<div class="modal">
+				<div class="blog-detail">
+					<figure><img src="img/blog-detail.jpg" alt=""><span>Feb 27, 2020</span></figure>
+					<div class="detail-meta">
+						<h2>Bootstrap is the Most Popular HTML, CSS, and JS Framework</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a fringilla elit, eu mattis arcu. Curabitur augue augue, scelerisque sit amet varius eget, tristique at nisl. In euismod suscipit tristique. Nulla sollicitudin pretium massa sit amet tristique.</p>
+						<blockquote>
+							<p>
+								" Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma - which is living with the results of other people's thinking"
+							</p>
+						</blockquote>
+						<p>
+							Praesent ac quam sed magna gravida interdum. Vivamus sed nunc lobortis, tempor ante ut, gravida nibh. Praesent sed euismod odio. Maecenas dui dui, euismod quis lobortis quis, dignissim ut eros. Curabitur cursus lacinia sollicitudin. Aenean vitae erat imperdiet, egestas nisi non, placerat sapien. In nec commodo justo. Nunc posuere lacinia neque eu malesuada. Sed lacus lacus, tincidunt id faucibus eget, sagittis sed enim. Nunc nec dapibus odio.
+						</p>
+						<h4>Share</h4>
+						<ul class="social-media">
+							<li><a href="#"><i class="icofont-facebook"></i></a></li>
+							<li><a href="#"><i class="icofont-instagram"></i></a></li>
+							<li><a href="#"><i class="icofont-twitter"></i></a></li>
+							<li><a href="#"><i class="icofont-pinterest"></i></a></li>
+							<li><a href="#"><i class="icofont-tumblr"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				<svg class="modal-svg" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" preserveAspectRatio="none">
+					<rect x="0" y="0" fill="none" width="100%" height="100%" rx="3" ry="3"></rect>
+				</svg>
+			</div>
+		</div>
+	</div><!-- modal popup blog detail -->
+
+	<script src="js/jquery.js"></script>
+	<script src="js/fullpage.js"></script>
+	<script src="js/typed.js"></script>
+	<script src="js/perfect-scrollbar.jquery.min.js"></script>
+	<script src="js/YTPlayer.min.js"></script>
+	<script src="../../../www.google.com/recaptcha/api.js"></script>
+	<script src="js/main.js"></script>
+
+</body>
+
+<!-- Mirrored from wpkixx.com/html/Aris/index-dark.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 27 Aug 2024 11:41:41 GMT -->
+
+</html>
